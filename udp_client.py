dc7de4fb9f03c9aadd822a5fc99c12aa4653d9b9
TheSmoothWater <thesmoothwater@gmail.com>
2025-02-16 18:14:43 -0500

    Works on my end

--- udp_client.py
+++ udp_client.py
@@ -3,7 +3,7 @@ import hashlib  # needed to calculate the SHA256 hash of the file
 import sys  # needed to get cmd line parameters
 import os.path as path  # needed to get size of file in bytes
 
-
+# '10.33.20.21'
 IP = '127.0.0.1'  # change to the IP address of the server
 PORT = 12000  # change to a desired port number
 BUFFER_SIZE = 1024  # change to a desired buffer size
@@ -22,13 +22,26 @@ def get_file_size(file_name: str) -> int:
 def send_file(filename: str):
     # get the file size in bytes
     # TODO: section 2 step 2 in README.md file
-
+    file_size = path.getsize(filename)
     # convert the file size to an 8-byte byte string using big endian
     # TODO: section 2 step 3 in README.md file
+    byte_size_object = file_size.to_bytes(8, byteorder='big')
+
+
+    # For when you forget how any of it works
+    # print(f"{byte_size_object}")
+    # byte_size_string = f"{byte_size_object}"
+    # bytesStringBackToBytes = bytes.fromhex(byte_size_string[2:-1].replace("\\x",""))
+    # print("Type Str back to bytes: ", type(bytesStringBackToBytes))
+    # print("String Back to Bytes: ", bytesStringBackToBytes)
+    # print("Byte Size String: ", byte_size_string)
+    # print("Int Size: ", int.from_bytes(bytesStringBackToBytes, byteorder="big"))
+    # print("Int Size: ", file_size)
+
 
     # create a SHA256 object to generate hash of file
     # TODO: section 2 step 4 in README.md file
-
+    myHash = hashlib.new('sha256')
     # create a UDP socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -36,22 +49,45 @@ def send_file(filename: str):
         # send the file size in the first 8-bytes followed by the bytes
         # for the file name to server at (IP, PORT)
         # TODO: section 2 step 6 in README.md file
-
+        sendSizeName = f"{byte_size_object},{filename}".encode()
+        client_socket.sendto(sendSizeName, (IP, PORT))
         # TODO: section 2 step 7 in README.md file
+        data = client_socket.recv(BUFFER_SIZE).decode()
+
+        if data != 'go ahead':
+            raise Exception('Bad server response - was not go ahead!')
 
         # open the file to be transferred
-        with open(file_name, 'rb') as file:
+        with open(filename, 'rb') as file:
             # read the file in chunks and send each chunk to the server
+            print("We opened the file")
             # TODO: section 2 step 8 a-d in README.md file
-            pass  # replace this line with your code
+            # replace this line with your code
+            chunkSize = BUFFER_SIZE
+            while True:
+                chunk = file.read(chunkSize)
+                myHash.update(chunk)
+                client_socket.sendto(chunk,(IP, PORT))
+                if not chunk:
+                    print("We broke out")
+                    break
+                data = client_socket.recv(BUFFER_SIZE).decode()
+                if data != 'received':
+                    raise Exception('Bad server response - was not received')
 
         # send the hash value so server can verify that the file was
         # received correctly.
         # TODO: section 2 step 9 in README.md file
-
+        print(f'Hash Digest = {myHash.digest()}')
+        client_socket.sendto(myHash.digest(), (IP, PORT))
         # TODO: section 2 steps 10 in README.md file
-
+        recvResult = client_socket.recv(BUFFER_SIZE).decode()
         # TODO: section 2 step 11 in README.md file
+        if recvResult != 'success':
+            print("The hash is wrong")
+            raise Exception('Transfer failed!')
+        print('Transfer completed!')
+
     except Exception as e:
         print(f'An error occurred while sending the file: {e}')
     finally:
@@ -60,8 +96,9 @@ def send_file(filename: str):
 
 if __name__ == "__main__":
     # get filename from cmd line
-    if len(sys.argv) < 2:
-        print(f'SYNOPSIS: {sys.argv[0]} <filename>')
-        sys.exit(1)
-    file_name = sys.argv[1]  # filename from cmdline argument
-    send_file(file_name)
+    # if len(sys.argv) < 2:
+    #     print(f'SYNOPSIS: {sys.argv[0]} <filename>')
+    #     sys.exit(1)
+    # file_name = sys.argv[1]  # filename from cmdline argument
+    # send_file(file_name)
+    send_file('fileTransferProtocol.png')
--- udp_server.py
+++ udp_server.py
@@ -9,21 +9,38 @@ BUFFER_SIZE = 1024  # change to a desired buffer size
 
 
 def get_file_info(data: bytes) -> (str, int):
-    return data[8:].decode(), int.from_bytes(data[:8],byteorder='big')
+    return data[8:].decode(), int.from_bytes(data[:8], byteorder='big')
 
 
 def upload_file(server_socket: socket, file_name: str, file_size: int):
     # create a SHA256 object to verify file hash
     # TODO: section 1 step 5 in README.md file
-
+    myHash = hashlib.new('sha256')
     # create a new file to store the received data
     with open(file_name+'.temp', 'wb') as file:
-        # TODO: section 1 step 7a - 7e in README.md file
-        pass  # replace this line with your code for section 1 step 7a - 7e
+        while True:
+            # TODO: section 1 step 7a - 7e in README.md file
+            # replace this line with your code for section 1 step 7a - 7e
+            data, address = server_socket.recvfrom(BUFFER_SIZE)
+            if not data:
+                print("We made it out")
+                break
+            file.write(data)
+            myHash.update(data)
+            server_socket.sendto(b'received', address)
 
     # get hash from client to verify
+    print("myHash: ", myHash.digest())
     # TODO: section 1 step 8 in README.md file
+    data, address = server_socket.recvfrom(BUFFER_SIZE)
     # TODO: section 1 step 9 in README.md file
+
+    if myHash.digest() == data:
+        print('Success')
+        server_socket.sendto(b'success', address)
+    else:
+        print('Failed')
+        server_socket.sendto(b'failed', address)
 
 
 def start_server():
@@ -36,13 +53,22 @@ def start_server():
         while True:
             # TODO: section 1 step 2 in README.md file
             # expecting an 8-byte byte string for file size followed by file name
+            recvBytes, address = server_socket.recvfrom(BUFFER_SIZE)
+            print("We received File Name")
             # TODO: section 1 step 3 in README.md file
+            file_info = recvBytes.decode().split(",")
+            bytesStringBackToBytes = bytes.fromhex(file_info[0][2:-1].replace("\\x", ""))
+            file_size = int.from_bytes(bytesStringBackToBytes, byteorder="big")
+            file_name = file_info[1]
             # TODO: section 1 step 4 in README.md file
+            server_socket.sendto(b'go ahead', address)
             upload_file(server_socket, file_name, file_size)
+
     except KeyboardInterrupt as ki:
         pass
     except Exception as e:
-        print(f'An error occurred while receiving the file:str {e}')
+        # I added \n so this could be a error in codegrade
+        print(f'An error occurred while receiving the file:str\n{e}')
     finally:
         server_socket.close()
 
